11b3f577d3eec5e73735c116d2b3698bfcc5308a
Teste <angelanogueirarafaelfilho@gmail.com>
2022-08-02 21:46:21 -0300

    metodos de Strings

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,27 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-          funcaoSoma(2,2);
+        funcaoSoma(2, 2);
+        funcaoSubtrair(2, 5);
+        List<String> x = nomesDeAlunos("Pedro", "Paulo");
+        for (String s : x) {
+            System.out.println();
+        }
     }
-
+    public static List<String> nomesDeAlunos(String s, String str){
+        List<String> listString = new ArrayList<>();
+        listString.add(s);
+        listString.add(str);
+        return listString;
+    }
+    public static void funcaoSubtrair(double x,double y){
+        double z = x - y;
+    }
     public static void funcaoSoma(int x, int y){
       int z = x+y;
         System.out.println("A soma de "+ x+" mais "+y+" é igual a "+z);
